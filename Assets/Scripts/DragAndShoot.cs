4ad4fa35abec4172af0752ec7d427888c479a419
Emre Özay <emreozay@mail.com>
2022-12-28 12:43:05 +0300

    Better movement script

--- Assets/Scripts/DragAndShoot.cs
+++ Assets/Scripts/DragAndShoot.cs
@@ -12,102 +12,62 @@ public class DragAndShoot : MonoBehaviour
     [SerializeField]
     private float forceMultiplier = 3f;
 
-    private Vector3 mousePressDownPos;
-    private Vector3 mouseReleasePos;
-
-    private Vector3 first;
+    private Vector3 firstPosition;
+    private Vector3 lastPosition;
+    private Vector3 shootDirection;
 
     private Rigidbody rb;
 
+    private float viewportXPosition;
     private bool isShoot;
 
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                mousePressDownPos = hit.point.normalized;
-                rb.velocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
 
-                first = mainCamera.ScreenToViewportPoint(Input.mousePosition);
-            }
+            lineRenderer.positionCount = 2;
+            firstPosition = new Vector3(transform.position.x, 0, transform.position.z);
+            lineRenderer.SetPosition(0, firstPosition);
+        }
 
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 newPos = hit.point;
-                newPos.y = 0;
+        if (Input.GetMouseButton(0))
+        {
+            RenderLine();
+        }
 
-                RenderLine(newPos);
-            }
+        if (Input.GetMouseButtonUp(0))
+        {
+            lineRenderer.positionCount = 0;
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                lineRenderer.positionCount = 0;
-
-                mouseReleasePos = hit.point.normalized;
-                mouseReleasePos.z = Mathf.Abs(mouseReleasePos.z);
-
-                Vector3 shootDirection = (mouseReleasePos - mousePressDownPos).normalized;
-                Shoot(hit.point);
-            }
+            Shoot();
         }
     }
 
-    private void RenderLine(Vector3 endPosition)
+    private void RenderLine()
     {
-        lineRenderer.positionCount = 2;
-        lineRenderer.SetPosition(0, new Vector3(transform.position.x, 0, transform.position.z));
-        /*
-        Vector3 firstPosition = transform.position;
-        firstPosition.y = 0;
-        firstPosition.z = Mathf.Abs(firstPosition.z);
-        lineRenderer.SetPosition(0, firstPosition);
-
-        Vector3 lastPosition = (endPosition - mousePressDownPos).normalized * 5f;
+        viewportXPosition = mainCamera.ScreenToViewportPoint(Input.mousePosition).x;
+        lastPosition.x = Mathf.Lerp(-10f, 10f, viewportXPosition);
         lastPosition.y = 0;
-        lastPosition.z = 10;
-        lineRenderer.SetPosition(1, firstPosition + lastPosition);*/
+        lastPosition.z = transform.position.z + 10;
 
-        /*Vector3 finalPos = transform.position + (endPosition * 10f);
-        finalPos.y = 0;
-        finalPos.z = transform.position.z + 10;
-        lineRenderer.SetPosition(1, finalPos);*/
-
-        Vector3 finalPos = mainCamera.ScreenToViewportPoint(Input.mousePosition);
-        finalPos.x = Mathf.Lerp(-10f, 10f, finalPos.x);
-        finalPos.y = 0;
-        finalPos.z = transform.position.z + 10;
-        
-        first.x = finalPos.x - lineRenderer.GetPosition(0).x;
-        lineRenderer.SetPosition(1, finalPos);
+        lineRenderer.SetPosition(1, lastPosition);
     }
 
-    void Shoot(Vector3 Force)
+    void Shoot()
     {
-        /*if (isShoot)
-            return;*/
-
-        //rb.AddForce(new Vector3(Force.x * 10f, 0, 10) * forceMultiplier, ForceMode.Impulse);
         isShoot = true;
 
-        Vector3 finalPos = mainCamera.ScreenToViewportPoint(Input.mousePosition) /*- first*/;
-        
-        finalPos.x = Mathf.Lerp(-10f, 10f, finalPos.x);
-        finalPos.y = 0;
-        finalPos.z = 10;
-
-        first.z = 10;
-        first.y = 0;
-        rb.AddForce(first * forceMultiplier, ForceMode.Impulse);
+        shootDirection.x = lastPosition.x - firstPosition.x;
+        shootDirection.y = 0;
+        shootDirection.z = 10;
+        rb.AddForce(shootDirection * forceMultiplier, ForceMode.Impulse);
     }
 }
